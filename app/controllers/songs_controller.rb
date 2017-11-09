92c4077c90feee4661a9720c61bcc7b5304f5225
Amanda Wehrman <arwehrman@gmail.com>
2017-11-09 03:00:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,23 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).posts
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
+      end
     else
-    @songs = Song.all
-  end
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    @song = Song.find_by(id: params[:id])
+    if @song.nil?
+      redirect_to artist_songs_path(params[:artist_id]), alert: "Song not found"
+    end
+
   end
 
   def new
